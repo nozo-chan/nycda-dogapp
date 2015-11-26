a55255b5bd4d7d6cb6d872179bb77f2ea926f66b
Mai Berger <m.c.berger@icloud.com>
2015-11-26 14:49:17 +0100

    added test for age method

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -2,44 +2,43 @@ require 'rails_helper'
 
 RSpec.describe Dog, :type => :model do
 
-	describe "is invalid without a name" do
-		it "name" do
-		dog = build(:dog)
-      	dog.name = nil
-      	expect(dog).to be_invalid
-      end
-  	end
+  describe "is invalid without a " do
+    it "name" do
+      dog = build(:dog)
+      dog.name = nil
+      expect(dog).to be_invalid
+    end
 
-	describe "is invalid without a birthday" do
-		it "birthday" do
-		dog = build(:dog)
-      	dog.birthday = nil
-      	expect(dog).to be_invalid
-      end
-  	end
+    it "birthday" do
+      dog = build(:dog)
+      dog.birthday = nil
+      expect(dog).to be_invalid
+    end
 
-  		describe "is invalid without an email" do
-		it "email" do
-		dog = build(:dog)
-      	dog.email = nil
-      	expect(dog).to be_invalid
-      end
-  	end
+    it "email" do
+      dog = build(:dog)
+      dog.email = nil
+      expect(dog).to be_invalid
+    end
 
-  		describe "is invalid without a city" do
-		it "city" do
-		dog = build(:dog)
-      	dog.city = nil
-      	expect(dog).to be_invalid
-      end
-  	end
+    it "city" do
+      dog = build(:dog)
+      dog.city = nil
+      expect(dog).to be_invalid
+    end
+    
+    it "state" do
+      dog = build(:dog)
+      dog.state = nil
+      expect(dog).to be_invalid
+    end
+  end
 
-  		describe "is invalid without a state" do
-		it "state" do
-		dog = build(:dog)
-      	dog.state = nil
-      	expect(dog).to be_invalid
-      end
-  	end
+  describe " testing for Dog methods " do 
+    it "age" do
+      dog = build(:dog) 
+      expect(dog.age).to eq(3)
+    end
+  end
 
 end
